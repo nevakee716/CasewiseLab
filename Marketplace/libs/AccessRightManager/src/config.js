9a0b8cf64df2ab1f6445aabbf44d87910fe4a24a
nevakee716 <nevakee716@gmail.com>
2019-01-05 18:03:36 +0000

    changing config

--- Marketplace/libs/AccessRightManager/src/config.js
+++ Marketplace/libs/AccessRightManager/src/config.js
@@ -1,13 +1,13 @@
 var configEditForbiddenPropertiesByViewAndRole = {
   process: { // nom de la vue
-    1: ['name'] // id du role, et tableau avec les propriétés impossible à modifier
+    1: ['name','type'] // id du role, et tableau avec les propriétés impossible à modifier
   }
 };
 
 var configReadForbiddenPropertiesByViewAndRole = {
   process: { // nom de la vue
     process : { // scriptname de l'objet type
-      1: ['type'] // id du role, et tableau avec les propriétés impossible à lire
+      1: ['criticité'] // id du role, et tableau avec les propriétés impossible à lire
     }
   }
 };
